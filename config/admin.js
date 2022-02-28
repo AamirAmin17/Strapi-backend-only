811f003790f78ba2956ef532dbe1b791056bc1de
Aamir Muhammad Amin <aamir1401a@gamil.com>
2022-02-28 12:42:22 +0500

    changed admin config

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'c80c35316451706eb2b8d20b06ee97a7'),
+    secret: env("JWT_SECRET", "c80c35316451706eb2b8d20b06ee97a7"),
   },
 });
